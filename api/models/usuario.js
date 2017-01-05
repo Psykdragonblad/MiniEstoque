4026d39b8fa40ef15ad395448e157b4b0712423b
Marcos Souza <marcos.souza14@hotmail.com>
2017-01-05 19:23:00 -0300

    Removendo o __v

--- api/models/usuario.js
+++ api/models/usuario.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var EstoqueSchema = new mongoose.Schema({
     _id: String,
     login: String,
-    senha: String
+    senha: String,
+    __v: { type: Number, select: false}
 },{collection:'usuario'});
 
 module.exports = mongoose.model('usuario', EstoqueSchema);
